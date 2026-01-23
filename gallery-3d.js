c02010cdcd73744fbb7ab5bf3527ceb3df9fb7be
Alejandro <areinosoperez@gmail.com>
2026-01-23 11:53:15 +0000

    Adjust carousel position to center images better with text

--- gallery-3d.js
+++ gallery-3d.js
@@ -91,7 +91,7 @@ window.initGallery3D = function() {
   }
 
   galleryGroup.rotation.y = CONFIG.wallAngleY;
-  galleryGroup.position.x = 8;
+  galleryGroup.position.x = 15;
 
   let currentScroll = 0;
   let targetScroll = 0;
